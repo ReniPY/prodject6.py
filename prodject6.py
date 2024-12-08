668097a576ae318f097f649c541a2f3ca0c05a8e
ReniPY <deserts876@gmail.com>
2024-12-08 21:00:23 +0700

    Add files via upload

---
+++ prodject6.py
@@ -0,0 +1,42 @@
+password = input("Введите пароль:")
+
+
+def is_very_long(password):
+    return len(password) >= 12
+
+
+def has_digit(password):
+    return any(symbol.isdigit() for symbol in password)
+
+
+def has_letters(password):
+    return any(symbol.isalpha() for symbol in password)
+
+
+def has_upper_letters(password):
+    return any(symbol.isupper() for symbol in password)
+
+
+def has_lower_letters(password):
+    return any(symbol.islower() for symbol in password)
+
+
+def has_symbols(password):
+    return any(symbol.isupper() and symbol.isalpha() for symbol in password)
+
+
+checks = [
+    has_digit,
+    is_very_long,
+    has_letters,
+    has_upper_letters,
+    has_lower_letters,
+    has_symbols,
+]
+
+
+count = 0
+for check in checks:
+    if check(password):
+        count += 2
+print(count)
